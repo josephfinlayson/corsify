9b49cf27ae5326dc134ebaf9fa8a1c25672d69d6
Joseph Finlayson <joseph.finlayson@gmail.com>
2018-07-02 16:42:03 +0200

    remove type

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-va// Listen on a specific host via the HOST environment variable
+// Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
